24fa64b1cfcb2de3758ea16f7b76dff00d679841
zhoujm <zhoujm1@yonyou.com>
2024-07-22 19:07:29 +0800

    消息入库优化

--- src/main/java/com/yonyougov/bootchat/qianfan/ChatController.java
+++ src/main/java/com/yonyougov/bootchat/qianfan/ChatController.java
@@ -1,23 +1,20 @@
 package com.yonyougov.bootchat.qianfan;
 
-import com.yonyougov.bootchat.base.user.User;
 import com.yonyougov.bootchat.fw.context.SessionContext;
-import com.yonyougov.bootchat.qianfan.dto.ChatMessage;
 import com.yonyougov.bootchat.qianfan.dto.ChatMessage2;
 import com.yonyougov.bootchat.qianfan.service.QianfanService;
 
-import org.springframework.ai.chat.messages.AssistantMessage;
-import org.springframework.ai.chat.messages.MessageType;
-import org.springframework.ai.chat.messages.UserMessage;
+import com.yonyougov.bootchat.vo.WebResult;
 import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.qianfan.QianFanChatModel;
+import org.springframework.ai.zhipuai.ZhiPuAiChatModel;
+import org.springframework.ai.zhipuai.ZhiPuAiImageModel;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RequestMapping("/qianfan")
 @RestController
@@ -25,11 +22,13 @@ public class ChatController {
 
     private final QianFanChatModel chatClient;
     private final QianfanService qianFanService;
+    private final ZhiPuAiImageModel zhiPuAiImageModel;
     private final SessionContext sessionContext;
 
-    public ChatController(QianFanChatModel chatClient, QianfanService qianFanService, SessionContext sessionContext) {
+    public ChatController(QianFanChatModel chatClient, QianfanService qianFanService, ZhiPuAiImageModel zhiPuAiImageModel, SessionContext sessionContext) {
         this.chatClient = chatClient;
         this.qianFanService = qianFanService;
+        this.zhiPuAiImageModel = zhiPuAiImageModel;
         this.sessionContext = sessionContext;
     }
 
@@ -38,7 +37,7 @@ public class ChatController {
         return Map.of("generation", chatClient.call(message));
     }
 
-//        @PostMapping("/ai/generateStream")
+    //        @PostMapping("/ai/generateStream")
 //    public Flux<ChatResponse> generateStream(@RequestBody List<ChatMessage> messages) {
 //        qianFanService.stream();
 //        Prompt prompt = new Prompt(
@@ -51,6 +50,14 @@ public class ChatController {
 //                }).collect(Collectors.toList()));
 //        return chatClient.stream(prompt);
 //    }
+    @PostMapping("/ai/image")
+    public WebResult getImage(@RequestBody ChatMessage2 messages) {
+        String image = zhiPuAiImageModel.call(
+                new ImagePrompt(messages.getProblem())
+        ).getResult().getOutput().getUrl();
+        return WebResult.newSuccessInstance().putData("image", image);
+    }
+
     @PostMapping("/ai/generateStream")
     public Flux<ChatResponse> generateStream(@RequestBody ChatMessage2 messages) {
         String userId = sessionContext.getCurrentUser().getId();
